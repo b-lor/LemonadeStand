a100591bf6c354b7ced55f73ea1bf28275a67cfd
b-lor <blorwi@gmail.com>
2018-10-30 08:55:48 -0500

    Added games rules to program.

--- LemonadeStand/LemonadeStand/Game.cs
+++ LemonadeStand/LemonadeStand/Game.cs
@@ -8,5 +8,27 @@ namespace LemonadeStand
 {
     class Game
     {
+        public void StartGame()
+        {
+
+            GameRules();
+
+            Console.ReadLine();
+        }
+
+        public void GameRules()
+        {
+            Console.WriteLine("Welcome to Milwaukee's hottest Lemonade Stand");
+            Console.ReadLine();
+            Console.WriteLine("You have 'X' amount of days to sell as much lemonade as possible. You will set the number of days (minimum 7 days).\n");
+            Console.WriteLine("You'll have to purchase 4 main ingredients, lemons, sugar, plastic cups and ice cubes.\n");
+            Console.WriteLine("Ice cubes melt at the end of every day so you will have to purchase them each morning.\n");
+            Console.WriteLine("Lemons have a 36 hour shelf life - anything you purchase in the morning will go bad at the end of the next day.\n");
+            Console.WriteLine("Sugar and plastic cups have no shelf life.  Puchase as needed.\n");
+            Console.WriteLine("By default, there is an old lemonade recipe that you will use and a price for each of those cups. You can tweak the ingredients and price per your demands.\n");
+            Console.WriteLine("You have full control of inventory, pricing, quality control and recipe.\n");
+            Console.WriteLine("You'll have to deal with mother nature as well. Depending on the weather, you may sell more or less.\n");
+            Console.WriteLine("The more sun and higher temperature, the more you'll sell. The wetter and lower temperature, the less sales.\n");
+        }
     }
 }
--- LemonadeStand/LemonadeStand/Program.cs
+++ LemonadeStand/LemonadeStand/Program.cs
@@ -10,17 +10,18 @@ namespace LemonadeStand
     {
         static void Main(string[] args)
         {
-        // define rules to game
-        // select the number of days to run
-        // dislplay weather, inventory levels and bank account
-        // visit store to buy materials
-        // choose quantity of each material to use in lemonade
-        // Define customer, more/less likely to make purchase based on price/quality
-        // At the end of the day, display profit/loss statement
-        // Create multiple players
-        // integrate weather api based on current temp and forecast
-        
-
+            // define rules to game
+            // select the number of days to run
+            // dislplay weather, inventory levels and bank account
+            // visit store to buy materials
+            // choose quantity of each material to use in lemonade
+            // Define customer, more/less likely to make purchase based on price/quality
+            // At the end of the day, display profit/loss statement
+            // Create multiple players
+            // integrate weather api based on current temp and forecast
+            Game game = new Game();
+            game.StartGame();
+            Console.ReadKey();
         }
     }
 }
